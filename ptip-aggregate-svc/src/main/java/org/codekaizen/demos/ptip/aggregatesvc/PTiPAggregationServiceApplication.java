ca0cadeae974dc980da0519b6aa402407558829c
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-05-02 07:30:03 -0500

    initial def of aggregator service

---
+++ ptip-aggregate-svc/src/main/java/org/codekaizen/demos/ptip/aggregatesvc/PTiPAggregationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.codekaizen.demos.ptip.aggregatesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PTiPAggregationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PTiPAggregationServiceApplication.class, args);
+	}
+
+}
