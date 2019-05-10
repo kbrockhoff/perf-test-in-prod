3d8b1443a16099ac83fbef238f36fd2ca0c36d7c
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-05-10 12:01:43 -0500

    initial def of monitoring namespace

---
+++ k8s/create_monitoring.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+. ./kubectl_check.sh
+
+WORKING_DIR=`pwd`
+TARGET_NS=monitoring
+
+kubectl get namespace $TARGET_NS > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+    kubectl create namespace $TARGET_NS
+fi
+
+echo "Deploying Cluster Prometheus"
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_cluster_role.yml
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_configmap.yml
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_deploy.yml
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_svc.yml
+
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/influxdb_configmap.yml
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/influxdb_deploy.yml
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/influxdb_svc.yml
---
+++ k8s/destroy_monitoring.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+. ./kubectl_check.sh
+
+WORKING_DIR=`pwd`
+TARGET_NS=monitoring
+
+kubectl get namespace $TARGET_NS > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    kubectl delete namespace $TARGET_NS
+fi
+
