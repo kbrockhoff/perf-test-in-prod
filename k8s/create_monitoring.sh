eadab9b67e3583e022c4d6b0b2c8245ff4619255
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-05-11 09:24:51 -0500

    added prometheus cluster monitoring

--- k8s/create_monitoring.sh
+++ k8s/create_monitoring.sh
@@ -11,13 +11,21 @@ then
     kubectl create namespace $TARGET_NS
 fi
 
-echo "Deploying Cluster Prometheus"
+echo "  Deploying Cluster Node Exporter"
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/node_exporter_ds.yml
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/node_exporter_svc.yml
+
+echo "  Deploying Cluster State Metrics"
+kubectl apply -n $TARGET_NS -f $WORKING_DIR/kube_state_metrics.yml
+
+echo "  Deploying Cluster Prometheus"
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_cluster_role.yml
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_configmap.yml
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_storage.yml
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_deploy.yml
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/prometheus_svc.yml
 
+echo "  Deploying Cluster InfluxDB"
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/influxdb_configmap.yml
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/influxdb_deploy.yml
 kubectl apply -n $TARGET_NS -f $WORKING_DIR/influxdb_svc.yml
